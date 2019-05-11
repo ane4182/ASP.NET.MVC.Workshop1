67dab806d1a109ca763b9efe337a577bf5930125
ane4182 <ane4182@gmail.com>
2019-05-11 18:27:37 +0800

    mvc

---
+++ ASP.NET.MVC.Workshop1/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Asp_Net_MVC_Workshop1.Models;
+
+namespace Asp_Net_MVC_Workshop1.Controllers
+{
+    public class BookController : Controller
+    {
+        Database1Entities1 db = new Database1Entities1();
+        // GET: Book
+        public ActionResult Index()
+        {
+            var book = db.BOOK_DATA.OrderBy(m => m.BOOK_CLASS_ID).ToList();
+            return View(book);
+        }
+       
+
+
+
+        
+
+    }
+}
